6d7b5614d2640feae3c9f18678c6f810144435f6
40Sabrina <sabrinaaaaa@hotmail.com>
2023-12-26 15:33:47 +0100

    Style_components finish

--- src/App.jsx
+++ src/App.jsx
@@ -29,20 +29,35 @@ const Etoile = style.div`
 display:flex;
 color: grey;
 `
-const Ul = style.ul`
+const Ul_1 = style.ul`
 display:flex;
 margin-right: 5%;
 padding: 0;
 font-size: 1em;
 list-style-type: none;
 `
-const Section_2 = style.h2`
+
+const Section_2 = style.div`
 margin: 0 5% 0 5%;
-color: orange;
-font-size: 1.2em;
-
+border-top: 1px solid white;
 `
-
+const Articles = style.article`
+display: flex;
+justify-content: space-around;
+`
+const A = style.a`
+color: white;
+text-decoration: none;
+background-color: #F1A53A;
+border-radius: 50px;
+padding: 10px;
+`
+const P = style.p`
+color: grey;
+text-align: center;
+padding-top: 5%;
+margin: 0;
+`
 
 function App() {
   return (
@@ -50,22 +65,29 @@ function App() {
       <Container>
         <Img src="https://th.bing.com/th/id/OIG.IEgTiSWPEalKcOw6MFoG?w=1024&h=1024&rs=1&pid=ImgDetMain"></Img>
         <Section_1>
-          <Title>Evasion</Title>
+          <Title>Lorem Ipsum</Title>
           <Etoile>
-            <Ul>
+            <Ul_1>
               <li>&#x2B50;</li>
               <li>&#x2B50;</li>
               <li>&#x2B50;</li>
               <li>&#x2B50;</li>
               <li>&#x2B50;</li>
-            </Ul>
+            </Ul_1>
             <p>4,5 (413)</p>
           </Etoile>
-          <h3>Secondary text</h3>
-          <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.</p>
+          <h3>What is Lorem Ipsum?</h3>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
         </Section_1>
         <Section_2>
-          <h2>Subtitle</h2>
+          <h3>Why do we use it?</h3>
+          <Articles>
+            <A href="">Article 1</A>
+            <A href="">Article 2</A>
+            <A href="">Article 3</A>
+            <A href="">Article 4</A>
+          </Articles>
+          <P>@Sabrina40</P>
         </Section_2>
       </Container >
     </>
